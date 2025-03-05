b26d62df5a036a1fb0c700f586b3a8c0d88a169d
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2025-03-05 18:56:14 +0900

    状態を画面で反映させる

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,16 +1,18 @@
 
-
+import { useRecoilValue } from "recoil";
+import { addTitleState, addTitleStateLength } from "./addTitleState";
 
 const AddTask: React.FC = () => {
-
+const addTask = useRecoilValue(addTitleState);
+const taskNumber = useRecoilValue(addTitleStateLength);
     return (
         <div>
-            <div>タスクが5個あります</div>
+            <div>タスクが{taskNumber}個あります</div>
             <ul>
-                {/* {taskList.map((task) => (
-                    <li key={task.id}>{task.text}</li>
-                ))} */}
-                <li>初めてのタスク</li>
+                {addTask.map((task) => (
+                    <li key={task.id}>{task.title}</li>
+                ))}
+                
             </ul>
         </div>
     );
